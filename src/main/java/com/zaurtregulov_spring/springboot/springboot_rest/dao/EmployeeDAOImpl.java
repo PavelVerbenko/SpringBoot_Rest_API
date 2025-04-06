3cbfb379b6dad0d8e573bd1d29dc4afbfed49863
username <ya.pasha.verbenko@yandex.ru>
2025-04-07 00:30:52 +0300

    Добавил devtools, для автоматического обновления на сервере томкат, так же переписал hibernate на jpa

--- src/main/java/com/zaurtregulov_spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov_spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -1,17 +1,14 @@
 package com.zaurtregulov_spring.springboot.springboot_rest.dao;
 
-
 import com.zaurtregulov_spring.springboot.springboot_rest.entity.Employee;
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import jakarta.persistence.Query;
 import java.util.List;
+
 @Repository
-@Transactional
 public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Autowired
@@ -20,9 +17,11 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public List<Employee> getAllEmployees() {
 
-        Session session = entityManager.unwrap(Session.class);
-
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        List<Employee> allEmployees = query.getResultList();
+        Query query = entityManager.createQuery("from Employee");
         List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
@@ -32,16 +31,19 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Transactional
     public void saveEmployee(Employee employee) {
 
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(employee);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(employee);
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
     }
 
     @Override
     @Transactional
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-
-        Employee employee = session.get(Employee.class, id);
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Employee employee = session.get(Employee.class, id);
+        Employee employee = entityManager.find(Employee.class, id);
         return employee;
     }
 
@@ -49,8 +51,11 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     @Transactional
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Query<Employee> query = session.createQuery("delete from Employee " + "where id =: employeeId");
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Employee> query = session.createQuery("delete from Employee " + "where id =: employeeId");
+//        query.setParameter("employeeId", id);
+//        query.executeUpdate();
+        Query query = entityManager.createQuery("delete from Employee " + "where id =: employeeId");
         query.setParameter("employeeId", id);
         query.executeUpdate();
     }
